c88e22327d9be8c1a53a232cf8d521bbd7da9d6e
danialwafiy <danialwafiy1014@gmail.com>
2022-10-10 11:10:13 +0800

    added h6 tag

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-export default function swapText(from, to) {
+function swapText(from, to) {
     if (from == undefined || to == undefined) {
         throw('from and to params is required.'); 
     }
 
-    const textElements = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
+    const textElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, li, td, caption, span, a')
 
     for (let i = 0; i < textElements.length; i++){
         if (textElements[i].innerHTML.toLowerCase().includes(from.toLowerCase())) {
@@ -11,3 +11,5 @@ export default function swapText(from, to) {
         }
     }
 }
+
+export default swapText
